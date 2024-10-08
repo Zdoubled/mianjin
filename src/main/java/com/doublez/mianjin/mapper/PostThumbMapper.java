561e858eab47b8aa7ed6bb8960b1555e1983ac01
z_double <1519595132@qq.com>
2024-10-09 00:27:48 +0800

    面金项目初始化模板

---
+++ src/main/java/com/doublez/mianjin/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.doublez.mianjin.mapper;
+
+import com.doublez.mianjin.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/doublez/mianjin/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.doublez.mianjin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.doublez.mianjin.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
